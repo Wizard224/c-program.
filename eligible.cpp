91dead444a7b8a64a6fe571fde2a7ccf6665ca73
Yash Parmar <66406874+Wizard224@users.noreply.github.com>
2020-06-27 22:32:56 +0530

    Add files via upload

---
+++ eligible.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio>
+void main()
+{
+clrscr();
+int age;
+cout<<"enter the age of the candidate:";
+cin>>age ;
+age>=18?cout<<"eligible for voting":cout<<"not eligible for voting";
+getch()
+}
